088fb104ee3044b1617c10396f45f208d8b2c920
FromSi <fromsi665@gmail.com>
2019-02-09 03:05:16 +0600

    Added tape_like.js

---
+++ README.md
@@ -0,0 +1,21 @@
+# О репозитории
+![](https://img.shields.io/github/forks/FromSi/TwitterScripts.svg?style=social) ![](https://img.shields.io/github/repo-size/FromSi/TwitterScripts.svg?style=flat) ![](https://img.shields.io/github/languages/top/FromSi/TwitterScripts.svg?style=flat)
+Когда-то я делал эксперименты над Twitter. Данные скрипты запускаются через консоль браузера.
+## tape_like.js ![](https://img.shields.io/github/size/FromSi/TwitterScripts/tape_like.js.svg?style=flat)
+Скрипт нужно запускать на главной странице Twitter'а (лента твиттов).
+Скрипт ставит лайки пользователям которые:
+* Подписанны на Вас;
+* Еще не получили вашего лайка на твит;
+* Создали свой твит, а не сделали ретвит.
+В скрипте достаточно изменить четыре переменные. 
+```javascript
+...
+// Ограничения для ленты.
+const MIN_TWEETS = 5;  		// мин твиттов на ленте
+const MAX_TWEETS = 100;	    // Макс твиттов на ленте
+
+// Сколько лайков и за какое время. 
+const timeHour = 8;      	// Сколько часов будем это крутить?
+const countLike = 500;   	// Сколько всего лайков нужно?
+...
+```
---
+++ tape_like.js
@@ -0,0 +1,130 @@
+// Лента твиттов.
+const ID_LIST_TWEETS = "stream-items-id";
+// Название класса. Кнопка для показа новых твиттов.
+const CLASS_BUTTON_NEW_TWEETS = "new-tweets-bar js-new-tweets-bar";
+// Название класса. Твитт из ленты.
+const CLASS_ITEM_TWEET = "js-stream-item stream-item stream-item";
+// Название класса. Проверка контекста на шапке твитта (ретвит, лайк и т.п.).
+const CLASS_HEAD_TWEET_CONTEXT = "context";
+// Название класса. Кнопка не нажатого лайка.
+const CLASS_BUTTON_LIKE = "ProfileTweet-actionButton js-actionButton js-actionFavorite";
+// Твитт, <div> и там находится этот аргумент. Подписал ли на меня данный аккаунт или нет.
+const ATTRIBUTE_FOLLOWS_ME = "data-follows-you";
+
+// Ограничения для ленты.
+const MIN_TWEETS = 5;  		// мин твиттов на ленте
+const MAX_TWEETS = 100;		// Макс твиттов на ленте
+
+// Сколько лайков и за какое время. 
+const timeHour = 8;      		// Сколько часов будем это крутить?
+const countLike = 500;   		// Сколько всего лайков нужно?
+
+// Перевожу в часы и узнаю задержку в секундах;
+var hostPause = ((timeHour * 60 * 60) / countLike) ^ 0;
+
+// Просто паузы, чтобы успевало все подгрузится.
+var loadTweetPause = 5000;
+var handlerTweetPause = 5000;
+
+// Счетчик твиттов. Помогает вычислить количество твиттов для MAX_TWEETS.
+var countTweets;
+
+host();
+
+// Избегаем рефлексию. :D
+function host(){
+    setTimeout(
+        () => main(), 
+        hostPause
+    );
+}
+
+// Нажимает на кнопку, для обновления твиттов и начинает их обрабатывать.
+function main(){
+	clickNewTweets();
+    
+    setTimeout(
+        () => handlerTweets(), 
+        loadTweetPause
+    );
+}
+
+// Обработчик ленты Twitter.
+function handlerTweets(){
+
+    getArrayTweet(
+        arr => {
+            if (arr.length < MIN_TWEETS){
+                reloadArrayTweet();
+            } else {
+            	clickLike(arr);
+                host();
+            }
+        }
+    );
+}
+
+// Повторить поиск твиттов.
+function reloadArrayTweet(){
+	clickNewTweets();
+    window.scrollTo(0, window.scrollMaxY);
+    window.scrollTo(0, 0);
+
+    setTimeout(
+        () => handlerTweets(), 
+        handlerTweetPause
+    );
+}
+
+// Удаляет не нужные твитты и скидывает callback с подходящими твиттами.
+function getArrayTweet(callback){
+    countTweets = 0;
+
+    var root = document.getElementById(ID_LIST_TWEETS);
+    var arrOld = Array.from(root.getElementsByClassName(CLASS_ITEM_TWEET));
+    var arrFilter = arrOld.filter(checkContext);
+
+    removeTweet(arrOld, arrFilter, root);
+        
+    callback(arrFilter);
+}
+
+// Найти собственные твитты пользователей и чтобы они меня читали.
+function checkContext(li){
+    if(li.getElementsByClassName(CLASS_HEAD_TWEET_CONTEXT)[0] == null ||
+            li.getElementsByTagName("ol")[0] != null ||
+            window.getComputedStyle(li.getElementsByClassName(CLASS_BUTTON_LIKE)[0], null).getPropertyValue("display") == "none"){
+        return false;
+    } else if (li.getElementsByClassName(CLASS_HEAD_TWEET_CONTEXT)[0].childElementCount == 0 &&
+                    JSON.parse(li.childNodes[1].getAttribute(ATTRIBUTE_FOLLOWS_ME)) &&
+                    countTweets < MAX_TWEETS) {
+        countTweets++;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Удаляет не нужные твитты.
+function removeTweet(arrOne, arrTwo, root){
+    arrOne.filter(element => !arrTwo.includes(element))
+                .forEach(element => root.removeChild(element));
+}
+
+
+// Возвращает случайное целое число между min (включительно) и max (не включая max)
+// Использование метода Math.round() даст вам неравномерное распределение!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Поставить лайк счастливчику.
+function clickLike(arr){
+	arr[getRandomInt(0, arr.length)].getElementsByClassName(CLASS_BUTTON_LIKE)[0].click();
+}
+
+// Показать новые твитты.
+function clickNewTweets(){
+	if (document.getElementsByClassName(CLASS_BUTTON_NEW_TWEETS)[0] != null)
+    	document.getElementsByClassName(CLASS_BUTTON_NEW_TWEETS)[0].click();
+}
